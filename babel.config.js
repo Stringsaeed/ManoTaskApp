9f1f57c5caef49757c0fdc7716fcd4bbe87e44b9
Muhammed Saeed <stringsaeed@gmail.com>
2023-08-13 02:01:58 +0400

    chore: setup project packages and modules

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['@rnx-kit/babel-preset-metro-react-native'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          components: './src/components',
+          constants: './src/constants',
+          hooks: './src/hooks',
+          navigation: './src/navigation',
+          routes: './src/routes',
+          screens: './src/screens',
+          services: './src/services',
+          types: './src/types',
+          configs: './src/constants',
+          i18n: './src/i18n',
+          views: './src/views',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
